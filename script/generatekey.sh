160936970d6430efe1cc642fab42ea4bb5b0fa0a
DangerBlack <danger.recheng@hotmail.it>
2016-09-13 23:24:43 +0200

    added generating keys autonomously

---
+++ script/generatekey.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+if [ ! -f ./server/plays/ssh/root.key ]; then
+    ssh-keygen -t rsa -N "" -f ./server/plays/ssh/root.key
+fi
+if [ ! -f ./server/plays/ssh/developer.key ]; then
+    ssh-keygen -t rsa -N "" -f ./server/plays/ssh/developer.key
+fi
