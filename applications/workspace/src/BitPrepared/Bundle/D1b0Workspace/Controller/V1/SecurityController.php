0170fd96a3ea0f613cd90e4d8ba8bdbe00e8e930
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-09-27 07:23:38 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- applications/workspace/src/BitPrepared/Bundle/D1b0Workspace/Controller/V1/SecurityController.php
+++ applications/workspace/src/BitPrepared/Bundle/D1b0Workspace/Controller/V1/SecurityController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Silex\Application;
 use Silex\Api\ControllerProviderInterface;
-use Monolog\Logger;
 use RedBeanPHP\Facade as R;
 
 class SecurityController implements ControllerProviderInterface
@@ -31,41 +30,41 @@ class SecurityController implements ControllerProviderInterface
     {
         /*TODO remove this line in producton DBG DATA {"authMode":"Email","email":"ugo.ugo@ugo.it","name":"ugo","surname":"ugo","password":"cane"}*/
         $data = json_decode($request->getContent(), true);
-        if($data === NULL){
-            $headers=[];
+        if ($data === NULL) {
+            $headers = [];
             $response = JsonResponse::create($res, 403, $headers)->setSharedMaxAge(300);
             return $response;
         }
 
         $authMode = $data['authMode'];
 
-        if($authMode === 'Email'){
+        if ($authMode === 'Email') {
             $email = $data['email'];
             $password = $data['password'];
             $name = $data['name'];
             $surname = $data['surname'];
-            $user = R::findOne('user',"WHERE email = ? AND name = ? AND surname = ?",[$email,$name,$surname]);
-            if($user->pwd === hash("sha256",$user->salt.$password)){
+            $user = R::findOne('user', "WHERE email = ? AND name = ? AND surname = ?", [$email, $name, $surname]);
+            if ($user->pwd === hash("sha256", $user->salt.$password)) {
                 //LOGGED IN!
                 $this->app['session']->set('user', ['id' => $user->id]);
-                $headers=[];
+                $headers = [];
                 $res = [
-                        "token"=>"blablabla",//TODO CREATE token
+                        "token"=>"blablabla", //TODO CREATE token
                         "clientId"=>$user->id
                 ];
                 $response = JsonResponse::create($res, 200, $headers)->setSharedMaxAge(300);
-            }else{
-                $headers=[];
+            }else {
+                $headers = [];
                 $res = [
                         "errore"=>"sbagliato password o user" //TODO roba
                 ];
                 $response = JsonResponse::create($res, 401, $headers)->setSharedMaxAge(300);
             }
         }
-        else{
+        else {
             //Facebook Redirect
         }
-        return $response;// JsonResponse::create($output, 200, $headers)->setSharedMaxAge(300);
+        return $response; // JsonResponse::create($output, 200, $headers)->setSharedMaxAge(300);
     }
     public function logout(Request $request)
     {
@@ -78,19 +77,19 @@ class SecurityController implements ControllerProviderInterface
     }
     public function confirm(Request $request)
     {
-            $confirmKey= $request->request->get('confirmKey');
-            $verify = R::findOne('verify',"WHERE key = ?",[$confirmKey]);
-            if (!$bean->id){
+            $confirmKey = $request->request->get('confirmKey');
+            $verify = R::findOne('verify', "WHERE key = ?", [$confirmKey]);
+            if (!$bean->id) {
                 //TODO mettere un controllo agli IP che forzano le richieste di token falsi
                 $response = "<html><head></head><body>Token non esistente!</body></html>";
-            }else{
-                if(strtotime($verify->inserttime) < strtotime("-15 minutes")){
-                    $user = R::load('user',$verify->user);
-                    $user->status="enabled";
-                    $user->updatetime=date('Y-m-d H:i:s');
+            }else {
+                if (strtotime($verify->inserttime) < strtotime("-15 minutes")) {
+                    $user = R::load('user', $verify->user);
+                    $user->status = "enabled";
+                    $user->updatetime = date('Y-m-d H:i:s');
                     $id = R::store($user);
                     $response = "<html><head></head><body>Account attivato complimenti!</body></html>";
-                }else{
+                }else {
                     $response = "<html><head></head><body>Impossibile attivare account inserire mail e password per richiedere un nuovo token!</body></html>";
                 }
             }
