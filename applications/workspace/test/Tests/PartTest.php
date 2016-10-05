d6f639958b61e617b0b80dd7916c3ccbfb6b8a78
DangerBlack <danger.recheng@hotmail.it>
2016-10-05 16:58:55 +0200

    UPDATE API part, still alive! still alive!

--- applications/workspace/test/Tests/PartTest.php
+++ applications/workspace/test/Tests/PartTest.php
@@ -26,4 +26,53 @@ class PartTest extends WebTestCase
         $assert = $this->evalValidation($validator);
         $this->assertTrue($assert);
     }
+
+
+    public function testPostWorkspacePart(){
+        //$schema = __DIR__.'/../../../../api/schemas/part.json';
+
+        $client = $this->createClient();
+        $client = $this->logIn($client);
+        $id = 50;
+        $part ='{"part":[
+                {
+                    "type": "image",
+                    "ref": "afaifnanabnawnfawba",
+                    "hash":"'.hash($id."prova0").'"
+                },
+                {
+                    "type": "image",
+                    "ref": "awfaowapaegbgepng",
+                    "hash":"'.hash($id."prova1").'"
+                },
+                {
+                    "type": "text",
+                    "ref": "afaafaafafafifnanabnawnfawba",
+                    "hash":"'.hash($id."prova2").'"
+                },
+                {
+                    "type": "video",
+                    "ref": "aaafawafaggagaagegaa",
+                    "hash":"'.hash($id."prova3").'"
+                }
+            ],
+            "badge":[13,28]}';
+
+        $client->request(
+          'POST',
+          '/api/v1/workspace/'.$id.'part',
+          [],
+          [],
+          ['CONTENT_TYPE' => 'application/json'],
+          $part);
+
+        $response = $client->getResponse();
+
+        //print_r($response);
+        $data = $client->getResponse()->getContent();
+        $validator = $this->askValidation($data,$schema);
+
+        $assert = $this->evalValidation($validator);
+        $this->assertTrue($assert);
+    }
 }
