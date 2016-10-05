3c1104b2a7ce14c90312b9eca80a1d6fee03d614
Stefano Tamagnini <yoghi@sigmalab.net>
2016-10-06 00:09:19 +0200

    stable server haproxy

---
+++ scripts/generateSSL.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ ! -f ./server/plays/ssl/selfsigned.crt ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.d1b0.local" -keyout ./server/plays/ssl/selfsigned.key -out ./server/plays/ssl/selfsigned.crt
+fi
